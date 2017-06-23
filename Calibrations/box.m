c7ab17a2d966fd8a3251925b7bf3d0921c64d697
adamimos <adamimos@gmail.com>
2017-06-23 11:30:34 -0700

    new calibrations

--- Calibrations/box.m
+++ Calibrations/box.m
@@ -10,7 +10,8 @@ port3_pulses = [205 200];
 plot(port3_time,port3_pulses,'o')
 
 % BOX 1
-% 2017.06.22 - 145 ms gets you 5 mL in 200 pulses for both ports
+% 2017.06.22 - 145 ms gets you 5 mL in 200 pulses for port 1
+% 135 for port 2
 
 port1_time = [145 120 130 140];
 port1_pulses = [200 230 220 205];
@@ -18,4 +19,27 @@ plot(port1_time,port1_pulses,'o')
 
 port3_time = [145 140 130 135];
 port3_pulses = [180 186 210 200];
+plot(port3_time,port3_pulses,'o')
+
+% BOX 2
+% 2017.06.22 - 145 ms gets you 5 mL in 200 pulses for both ports
+
+port1_time = [145 ];
+port1_pulses = [200];
+plot(port1_time,port1_pulses,'o')
+
+port3_time = [145 ];
+port3_pulses = [200 ];
+plot(port3_time,port3_pulses,'o')
+
+
+% BOX 3
+% 2017.06.22 - 145 ms gets you 5 mL in 200 pulses for both ports
+
+port1_time = [145 ];
+port1_pulses = [200];
+plot(port1_time,port1_pulses,'o')
+
+port3_time = [145 ];
+port3_pulses = [200 ];
 plot(port3_time,port3_pulses,'o')
