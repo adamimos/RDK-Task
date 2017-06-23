7f64536d590e6bde1cf616ef5cff7eafdd80d8fe
adamimos <adamimos@gmail.com>
2017-06-23 10:17:10 -0700

    calibration

---
+++ Calibrations/box.m
@@ -0,0 +1,11 @@
+% BOX 0
+% 2017.06.22 - 145 ms gets you 5 mL in 200 pulses for both ports
+
+port1_time = [100 115 125 135 145 150];
+port1_pulses = [375 300 260 235 200 186];
+
+plot(port1_time,port1_pulses,'o')
+
+port2_time = [140 145];
+port2_pulses = [205 200];
+plot(port2_time,port2_pulses,'o')
