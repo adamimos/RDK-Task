92aeb00066aa69a41b5fa9cf30357d192d0df7a9
adamimos <adamimos@gmail.com>
2017-08-29 08:39:40 -0700

    new infinite task

--- analysis.m
+++ analysis.m
@@ -12,14 +12,14 @@ clear all; close all; clc;
 
 
 
-rat_name = 'zandra1';
+rat_name = 'robin0';
 
 
 %date format should be yyyymmdd or * for all dates
-date = '201705*';
+date = '2017*';
 
 [list, dirs] = glob(strcat('C:/DATA/', date , '/', rat_name, '*.mat'));
-[list, dirs] = glob(strcat('C:/OLD_DATA/DATA/Adam', date , '/', rat_name, '*.mat'));
+%[list, dirs] = glob(strcat('C:/OLD_DATA/DATA/Adam', date , '/', rat_name, '*.mat'));
 objs = {};
 
 num_files = size(list, 1);
